2b38b723ea1a99f6c3f8ede9078c8ecf69891152
hamza <hamza427133@gmail.com>
2024-03-03 18:29:20 +0500

    new assignment

---
+++ app.js
@@ -0,0 +1,164 @@
+//Array
+
+// Q1
+
+var studentName = [];
+
+// Q2
+
+var studentName = [];
+
+// Q3
+
+var stringsArray = ["apple", "banana", "cherry"];
+
+// Q4
+
+var numArray = [1, 2, 3];
+
+// Q5
+
+var booleanArrey = [true, false];
+
+// Q6
+
+var mixArray = ["apple", 1, true];
+
+// Q7
+
+var qualification = ["SSC", "HSC", "BCS", "BS", "BECOM", "MS", "M.PHIL", "PHD"];
+
+document.write(qualification  + "<br>" + "<br>");
+
+// Q8
+
+var student1 = ["Score of Mchael is 320. percentage: 64%"];
+
+var student2 = ["Score of Jhone is 230. percentage: 46%"];
+
+
+var student3 = ["Score of tony is 480. percentage: 94%"];
+
+var studentPercentage =[student1 +  "<br>" + student2 + "<br>" + student3]
+
+
+document.write(studentPercentage)
+
+
+// Q9
+
+// Initialize an array with color names
+// var colors = ["red", "blue", "green", "yellow", "black"];
+
+// // Display the array elements in the browser
+// document.write(colors + "<br>" + "<br>");
+
+// // Part a: Add a color to the beginning of the array
+// var colorToAddBeginning = prompt("Enter a color to add to the beginning of the array:");
+// colors.unshift(colorToAddBeginning);
+// document.write("Updated array after adding a color to the beginning: " + colors + "<br>" + "<br>");
+
+// // Part b: Add a color to the end of the array
+// var colorToAddEnd = prompt("Enter a color to add to the end of the array:");
+// colors.push(colorToAddEnd);
+// document.write("Updated array after adding a color to the end: " + colors + "<br>" + "<br>");
+
+// // Part c: Add two colors to the beginning of the array
+// colors.unshift("white", "purple");
+// document.write("Updated array after adding two colors to the beginning: " + colors + "<br>" + "<br>");
+
+// // Part d: Delete the first color in the array
+// colors.shift();
+// document.write("Updated array after deleting the first color: " + colors + "<br>" + "<br>");
+
+// // Part e: Delete the last color in the array
+// colors.pop();
+// document.write("Updated array after deleting the last color: " + colors + "<br>" + "<br>");
+
+// // Part f: Add a color to a specific index
+// var indexToAdd = prompt("Enter the index where you want to add a color:");
+// var colorToAdd = prompt("Enter the color you want to add:");
+// colors.splice(indexToAdd, 0, colorToAdd);
+// document.write("Updated array after adding a color to a specific index: " + colors + "<br>" + "<br>");
+
+// // Part g: Delete colors from a specific index
+// var indexToDelete = prompt("Enter the index from where you want to delete colors:");
+// var numColorsToDelete = prompt("Enter the number of colors you want to delete:");
+// colors.splice(indexToDelete, numColorsToDelete);
+// document.write("Updated array after deleting colors from a specific index: " + colors + "<br>" + "<br>");
+
+
+// Q10
+
+
+var studentScores = [55, 80, 45, 75, 90, 60];
+
+document.write("score of student" + studentScores + "<br><br>");
+
+
+var studentOrder = [12, 45, 67, 35, 64, 23]
+
+document.write("order score of student " + studentOrder + "<br><br>")
+
+
+
+
+// Q11
+
+
+var cityList = ["karachi" , "lahore" ," peshawar"];
+
+
+cityList.push = ("islamabad" , "qutta" )
+
+console.log(cityList )
+
+
+
+// Q12
+
+var arr = ["this ",  " is " ,  " my " ,  " cat"];
+
+document.write( "<h1>Array:</h1>" + arr)
+
+
+
+var arr = ["this is my cat"];
+
+document.write( "<h1>Sring:</h1>" + arr + "<br><br>")
+
+
+//Q13
+
+var device = ["keybord" , "mouse" , "printer" , "moniter"]
+
+document.write("Device:" + "<br>" + device + "<br><br>" )
+
+
+
+var key = ["keyboard"];
+document.write("Out:" + "<br>" + key + "<br>");
+
+
+var mou = ["mouse"];
+document.write("Out:" + "<br>" + mou + "<br>");
+
+
+var pri = ["printer"];
+document.write("Out:" + "<br>" + pri + "<br>");
+
+
+var mon = ["moniter"];
+document.write("Out:" + "<br>" + mon + "<br><br>");
+
+
+
+// Q15
+
+var phoneList = ["Apple ", " Samsung ", " Motorola ", " Nokia "," Sony & Haier "]
+
+document.write( "PhoneList:" + "<br>" + phoneList)
+
+
+
+
